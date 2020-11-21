1550d573ef8c5966f32bd6da0e8c39ca73fa77b8
ReyBahl <bahl.reyansh@gmail.com>
2020-11-21 16:19:27 -0500

    Fixed appending problem to hashtable

--- hashtable.py
+++ hashtable.py
@@ -3,20 +3,24 @@ class HashTable():
 
     def __init__(self, length):
         self.length = length
-        self.table = [[]] * length
+        self.table = [[] for i in range(length)]
     
     def get_index(self, element):
         self.hashcode = hash(element)
         self.index = self.hashcode % self.length
 
         return self.index
+        
+    def __str__(self):
+        return str(self.table)
 
     def insert(self, element):
         self.table[self.get_index(element)].append(element)
-        
+    
     def contains(self, element_name):
         return (element_name in self.table[self.get_index(element_name)])
 
 mytable  =  HashTable(6)
 mytable.insert("Hello")
 print(mytable.contains("Bye"))
+print(mytable)
