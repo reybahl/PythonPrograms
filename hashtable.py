69732557fc4126ad7188fc25712ad4e4285e986b
ReyBahl <bahl.reyansh@gmail.com>
2020-11-23 10:13:42 -0500

    Added delete and locate functionality to hashtable

--- hashtable.py
+++ hashtable.py
@@ -14,13 +14,31 @@ class HashTable():
     def __str__(self):
         return str(self.table)
 
-    def insert(self, element):
-        self.table[self.get_index(element)].append(element)
+    def insert(self, element_name):
+        self.table[self.get_index(element_name)].append(element_name)
     
     def contains(self, element_name):
         return (element_name in self.table[self.get_index(element_name)])
+    
+    def delete(self, element_name):
+        index = self.get_index(element_name)
+        self.table[index].remove(element_name)
+    
+    def locate(self, element_name):
+        if self.contains(element_name):
+            index = self.get_index(element_name)
+            next_index = self.table[index].index(element_name)
 
+            print(f"{element_name} found at [{index}][{next_index}]")
+
+        else:
+            print("Element not found")
+
+#Tests
 mytable  =  HashTable(6)
 mytable.insert("Hello")
 print(mytable.contains("Bye"))
 print(mytable)
+mytable.locate("Hello")
+mytable.delete("Hello")
+mytable.locate("Hello")
